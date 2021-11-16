d53bf52c6e7ef93de67eee50906cdafc8b388a6d
elias7991 <78571779+elias7991@users.noreply.github.com>
2021-11-16 18:41:54 -0300

    form add cliente, falta validar

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 // AQUI IMPORTAMOS LA CLASE CLIENTE
-import {Cliente} from "../../models/cliente";
+import { Cliente } from "../../models/cliente";
+import { MatAccordion } from "@angular/material/expansion";
+import { FormControl, Validators } from "@angular/forms";
 
 
 @Component({
@@ -15,13 +17,63 @@ export class ClientesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+
   //instanciamos la clase Cliente
   arrayClientes: Cliente[] = [
-    {ruc: "1832131-2", nombre: "Elias Caceres", email: "ejemplo1@ejemplo.com"},
+    {ruc: "1832131-2", nombre: "Elias Cáceres", email: "ejemplo1@ejemplo.com"},
     {ruc: "1234567-1", nombre: "Ever Garay", email: "ejemplo2@ejemplo.com"},
     {ruc: "7654321-3", nombre: "Marcelo Molas", email: "ejemplo3@ejemplo.com"},
     {ruc: "6985346-2", nombre: "Carin Martínez", email: "ejemplo4@ejemplo.com"},
     {ruc: "1435597-2", nombre: "Melani Bazan", email: "ejemplo2@ejemplo.com"},
   ];
+
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  nombre = new FormControl(
+    '',
+    [
+      Validators.required,
+      Validators.maxLength(32),
+      Validators.minLength(3)
+    ]
+  );
+
+  ruc = new FormControl('', Validators.required);
+
+
+  // FormControl del campo email
+  email = new FormControl('', [Validators.required, Validators.email]);
+  // para mostrar el mensaje de error en el campo de email
+  getErrorMessage() {
+    if(this.email.hasError('required')) {
+      return 'Debe ingresar un correo electrónico';
+    }
+
+    return this.email.hasError('email') ? 'Correo inválido':'';
+  }
+
+  getErrorName(){
+    return this.nombre.hasError('required')?'Debe ingresar un nombre':'';
+  }
+
+  getErrorRuc(){
+    return this.ruc.hasError('required')?'Debe ingresar su ruc':'';
+  }
+
+  //funcion para guardar datos del formulario
 
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,7 +7,11 @@ import {MatGridListModule} from "@angular/material/grid-list";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-
+import {MatRadioModule} from "@angular/material/radio";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -23,7 +27,13 @@ import {MatButtonModule} from "@angular/material/button";
     MatGridListModule,
     MatListModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatRadioModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ComponentsModule { }
