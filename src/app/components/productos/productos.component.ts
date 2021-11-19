b29a7d22059272bd3ab1bb5eb70d49a7ac0b5a22
everfgmolinasFPUNA <everfgmolinas@fpuna.edu.py>
2021-11-19 18:32:14 -0300

    Validaciones

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { AppComponent } from 'src/app/app.component';
 
 import {Producto} from "../../models/producto";
 
@@ -23,10 +21,65 @@ export class ProductosComponent implements OnInit {
   selectedProducto: Producto = new Producto();
 
   addOrEdit(){
+    // Variable para control de error
+    let error = false;
+
+    //Si el producto no existe, o sea, si no es una edicion
     if(!this.productosArray.includes(this.selectedProducto)){
-      this.productosArray.push(this.selectedProducto);
+      //Buscar coincidenciad de código
+      for(let product of this.productosArray){
+        if(product.codigo == this.selectedProducto.codigo){
+          alert("Código de producto en existencia")
+          error = true;
+        }
+      }
+
+      //controlar siempre si ya no existe un error para no presentar varios errores
+
+      if(!error){
+        //Buscar coincidenciad de nombre
+        for(let product of this.productosArray){
+          if(product.nombre == this.selectedProducto.nombre){
+            alert("Nombre de producto ya existe")
+            error = true;
+          }
+        }
+      }
+      if(!error){
+        // Error de código
+        if(this.selectedProducto.codigo <= 0){
+          alert("Código no válido")
+          error = true;
+        }
+      }
+      if(!error){
+        // Error de nombre
+        if(this.selectedProducto.nombre == ''){
+          alert("Nombre no válido")
+          error = true;
+        }
+      }
+      if(!error){
+        // Error de precio
+        if(this.selectedProducto.precio <= 0){
+          alert("Precio no válido")
+          error = true;
+        }
+      }
+      if(!error){
+        // Error de cantidad
+        if(this.selectedProducto.existencia < 0){
+          alert("Cantidad no válida")
+          error = true;
+        }
+      }
+      if(!error){
+        this.productosArray.push(this.selectedProducto);
+      }
     }
-    this.selectedProducto = new Producto();
+    if(!error){
+      this.selectedProducto = new Producto();
+    }
   }
 
   openForEdit(producto: Producto){
